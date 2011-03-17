4caa766a30b4faee800db1c65f355e3a6d160d50
quix <quixoticsycophant@gmail.com>
2011-03-16 21:46:11 -0400

    add public method steamroll_ast for steamrolling individual asts; cleanup

--- lib/live_ast_ripper.rb
+++ lib/live_ast_ripper.rb
@@ -6,7 +6,7 @@ require 'ripper'
 #
 class LiveASTRipper
   #
-  # Returns a line-to-sexp hash where sexp corresponds to the method
+  # Returns a line-to-AST hash where the AST corresponds to the method
   # or block defined at the given line.
   #
   # This method is the only requirement of a LiveAST parser plugin.
@@ -17,52 +17,68 @@ class LiveASTRipper
     @defs
   end
 
-  def process(sexp)
-    line =
-      case sexp.first
-      when :def
-        sexp[1][2][0]
-      when :method_add_block
-        case sexp[1][0]
-        when :method_add_arg
-          sexp[1][1][1].last[0]
-        when :call, :command_call
-          sexp[1][3][2][0]
-        when :command
-          sexp[1][1][2][0]
-        end
-      end
-
-    if line
+  def process(sexp)  #:nodoc:
+    line = line_of(sexp) and
       @defs[line] = @defs.has_key?(line) ? :multiple : sexp
-    end
 
-    steamroll(sexp) if LiveASTRipper.steamroll
+    LiveASTRipper.steamroll_toplevel(sexp) if LiveASTRipper.steamroll
 
     sexp.each do |elem|
       process(elem) if elem.is_a? Array
     end
   end
 
-  def steamroll(sexp)
-    if sexp.first.is_a?(Symbol) and sexp.first[0] == "@"
-      # remove [line, column]
-      sexp.pop
-    end
-    
-    sexp.map! { |elem|
-      case elem
-      when :brace_block, :do_block
-        :block
-      else
-        elem
+  def line_of(sexp)  #:nodoc:
+    case sexp.first
+    when :def
+      sexp[1][2][0]
+    when :method_add_block
+      case sexp[1][0]
+      when :method_add_arg
+        sexp[1][1][1].last[0]
+      when :call, :command_call
+        sexp[1][3][2][0]
+      when :command
+        sexp[1][1][2][0]
       end
-    }
+    end
   end
 
   class << self
+    def steamroll_toplevel(sexp)  #:nodoc:
+      # remove [line, column]
+      if sexp.first.is_a?(Symbol) and sexp.first[0] == "@"
+        sexp.pop
+      end
+    
+      sexp.map! { |elem|
+        case elem
+        when :brace_block, :do_block
+          :block
+        else
+          elem
+        end
+      }
+    end
+
     #
-    # Whether to strip line/column and other personality traits.
+    # Destructive operation on +ast+:
+    #
+    # * remove [line, column] pairs
+    # * change +:brace_block+ and +:do_block+ to +:block+
+    #
+    def steamroll_ast(ast)
+      steamroll_toplevel(ast)
+      ast.each do |elem|
+        steamroll_ast(elem) if elem.is_a? Array
+      end
+      ast
+    end
+
+    #
+    # When true-valued, steamroll all ASTs created by the parser.
+    # Default is +false+. (Although referencing
+    # <code>LiveASTRipper::Test</code> will set it to +true+.)
     #
     attr_accessor :steamroll
   end
